0ed4cc4182dbce5bd9e8e83c52c77999fba7fd83
Philippelebg <zywu1968@hotmail.com>
2023-11-16 18:38:02 +0100

    a

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ const trackUrls = [
   'https://p.scdn.co/mp3-preview/ac28d1b0be285ed3bfd8e9fa5fad133776d7cf36',
 ];
 
-
+const [trackIndex, setTrackIndex] = useState(0);
 
 const goToNextTrack = () => {
   trackIndex += 1;
